3c6fb7d6784c8566b459953b3123a9ddafe8c1fa
leggy <alexapleyto@gmail.com>
2024-11-14 22:09:50 +0800

    Update items.h

--- MachineProjectTerm1/items.h
+++ MachineProjectTerm1/items.h
@@ -1,6 +1,8 @@
 //MAINTENANCE
 void viewItems(); //not sure if we have to add more to here
 void viewInventory();
+void modifyPrice(int *itemNumber, char (*itemNames)[11], float *itemPrice, int *stockLeft);
+void maintenanceMenu();
 
 //SILOG (temporary idk if we need this)
 void billDenomination();
